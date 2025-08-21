a9ac4f79d7072daef96c440acf04f0c471d7d8a9
Saharsh05 <22233592@bucks.ac.uk>
2025-08-21 09:30:37 +0000

    Added SupabaseClient.js and auth middleware

--- backend/services/supabaseClient.js
+++ backend/services/supabaseClient.js
@@ -1,1 +1,17 @@
- 
+ const {createClient} = require('@supabase/supabase-js');
+
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if(!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
+  throw new Error("Supabase URL and Service Role Key must be set in environment variables");
+}
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
+  auth: {persistSession: false}
+
+});
+
+// Export the Supabase client for use in other modules
+module.exports = supabase;
